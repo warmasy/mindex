6c15764eb55596e73dc58b80e122ba42a7a80403
warmasy <3139822307@qq.com>
2026-06-07 20:23:05 +0800

    update

--- vite/plugins/index.js
+++ vite/plugins/index.js
@@ -1,5 +1,4 @@
 import vue from '@vitejs/plugin-vue'
-import monacoEditorEsmPlugin from 'vite-plugin-monaco-editor-esm'
 
 import createAutoImport from './auto-import'
 import createSvgIcon from './svg-icon'
@@ -9,12 +8,8 @@ import createSetupExtend from './setup-extend'
 export default function createVitePlugins(viteEnv, isBuild = false) {
     const vitePlugins = [vue()]
     vitePlugins.push(createAutoImport())
-	vitePlugins.push(createSetupExtend())
-	vitePlugins.push(monacoEditorEsmPlugin({
-        languageWorkers: ['editorWorkerService'],
-        customWorkers: []
-    }))
+    vitePlugins.push(createSetupExtend())
     vitePlugins.push(createSvgIcon(isBuild))
-	isBuild && vitePlugins.push(...createCompression(viteEnv))
+    isBuild && vitePlugins.push(...createCompression(viteEnv))
     return vitePlugins
 }
